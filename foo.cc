126f9666085eb68823b07abc8488d04b11048fe8
ste <m@m.com>
2017-06-25 15:47:46 +0200

    Add the Foo.
    Change-Id: I01d7fa717121de97e56b3ac56d946dbd9db30909

---
+++ foo.cc
@@ -0,0 +1,9 @@
+#include "foo.h"
+
+Foo::Foo() {
+  x_ = 0;
+}
+
+int Foo::getX() {
+  return x_;
+}
---
+++ foo.h
@@ -0,0 +1,16 @@
+#ifndef FOO
+#define FOO
+
+// The Foo class.
+class Foo {
+ public:
+  Foo();
+
+  // Returns x.
+  int getX();
+
+ private:
+  int x_;
+}
+
+#endif  // FOO
